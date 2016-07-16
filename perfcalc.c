2712dbd4d9cc5252eff2345d782cb69647c37f9c
Tanguy <trouffignac@enst.fr>
2016-07-17 00:02:52 +0200

    Allows games to count for 2 points like in Top Jeunes

--- perfcalc.c
+++ perfcalc.c
@@ -3,14 +3,17 @@
 #include <time.h>
 #include "elotable.h"
 
-int number;
+int numberdouble;
+int numbersimple;
 int result;
 int opponents[100];
 int ratings[100];
 
 int getTheoricalScore(int value){
 	int score = 0;
-	for (int i = 0 ; i < number ; i ++)
+	for (int i = 0 ; i < numberdouble ; i ++)
+		score += 2 * getProbability(value - opponents[i]);
+	for (int i = numberdouble ; i < numberdouble + numbersimple ; i ++)
 		score += getProbability(value - opponents[i]);
 	return score;
 }
@@ -20,13 +23,13 @@ int getPerformance(int average){
 	int first;
 	int second;
 	if(result == 0){
-		for(int i = 0 ; i < number ; i ++)
+		for(int i = 0 ; i < numbersimple ; i ++)
 			if(opponents[i] < attempt)
 				attempt = opponents[i];
 		return attempt - 736;
 	}
-	if(result == 100 * number){
-		for(int i = 0 ; i < number ; i ++)
+	if(result == 100 * numbersimple){
+		for(int i = 0 ; i < numbersimple ; i ++)
 			if(opponents[i] > attempt)
 				attempt = opponents[i];
 		return attempt + 736;
@@ -74,27 +77,53 @@ int main(int argc, char * argv []){
 		printf("%d\n", getProbability(atoi(argv[1])));
 	}
 	else {
-		scanf("%d\n", &number);
+		scanf("%d %d\n", &numberdouble, &numbersimple);
 		scanf("%d\n", &result);
 		int theor = 0;
 		int sommeAdv = 0;
-		for(int i = 0 ; i < number ; i ++){
+		for(int i = 0 ; i < numberdouble ; i ++){
+			scanf("%d %d\n", &(ratings[i]), &(opponents[i]));
+			sommeAdv += opponents[i];
+			theor += 2 * getProbability(ratings[i] - opponents[i]);
+		}
+		for(int i = numberdouble ; i < numbersimple + numberdouble; i ++){
 			scanf("%d %d\n", &(ratings[i]), &(opponents[i]));
 			sommeAdv += opponents[i];
 			theor += getProbability(ratings[i] - opponents[i]);
 		}
-	if (theor % 100 >= 10)
-		printf("Score théorique	: %d.%d\n", theor / 100, theor % 100);
-	else
-		printf("Score théorique : %d.0%d\n", theor / 100, theor % 100);
-		printf("Performance : %d\n", getPerformance((float)sommeAdv/number));
+		if (theor % 100 >= 10)
+			printf("Score théorique	: %d.%d\n", theor / 100, theor % 100);
+		else
+			printf("Score théorique : %d.0%d\n", theor / 100, theor % 100);
+		printf("Performance : %d\n", getPerformance((float)sommeAdv/numbersimple));
 	}
 	int win = 0;
 	int draw = 0;
 	int loss = 0;
 	for (int i = 0 ; i < 1000000 ; i ++){
 		int score = 0;
-		for (int j = 0 ; j < number ; j ++){
+		for (int j = 0 ; j < numberdouble ; j ++){
+			int r = rand() % 200;
+			int proba = getProbability(opponents[j] - ratings[j]);
+			if (proba < 50){
+				if(r > proba){
+					if(r < 3 * proba){
+						score += 2;
+					} else {
+						score += 4;
+					}
+				}
+			} else {
+				proba = 100 - proba;
+				if(r < proba){
+					score += 4;
+				} else {
+					if(r < 3 * proba)
+						score += 2;
+				}
+			}
+		}
+		for (int j = numberdouble ; j < numberdouble + numbersimple ; j ++){
 			int r = rand() % 200;
 			int proba = getProbability(opponents[j] - ratings[j]);
 			if (proba < 50){
@@ -115,6 +144,7 @@ int main(int argc, char * argv []){
 				}
 			}
 		}
+		int number = 2 * numberdouble + numbersimple;
 		if (score > number)
 			win ++;
 		if (score == number)
